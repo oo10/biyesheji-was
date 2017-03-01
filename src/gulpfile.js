621e3d8fa81a206e0fc2d1aebe0cd5a6b7f0d07e
yangqiwang <simple11@vip.qq.com>
2017-03-01 19:49:13 +0800

    .

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -6,6 +6,17 @@
 var gulp = require('gulp'),
     jade = require('gulp-jade');
 
+var html2jade = require('gulp-html2jade');
+var options = {nspaces:2,donotencode:''};
+
+
+gulp.task('html2jade', function(){
+    gulp.src('**/*.html')
+        .pipe(html2jade(options))
+        .pipe(gulp.dest('dist'));
+});
+
+
 gulp.task('jade', function() {
     return gulp.src('**/*.jade')
         .pipe(jade())
@@ -13,8 +24,12 @@ gulp.task('jade', function() {
 });
 
 gulp.task('watch', function() {
-    gulp.watch('*/*.jade', ['jade']);
+    gulp.watch('*/*.html', ['html2jade']);
 });
+//
+// gulp.task('watch', function() {
+//     gulp.watch('*/*.jade', ['jade']);
+// });
 
 // gulp.task('watch', function() {
 //     gulp.watch('./**/*.jade', function(e) {
